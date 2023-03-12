5cf8adbd9012b99de70bdd327460c0e87712facc
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-12 18:05:20 +0600

    eventListener

--- eventt.js
+++ eventt.js
@@ -1,7 +1,9 @@
 console.log('hello to js dom events')
-
+// option 01 directly set on the html element
+// butttom onclick='console.log(67)'>Another button</button.
 
 // option 02 - add onclick function
+// <button onclick="makeRed()">Make Red</button>
 function makeRed(){
     document.body.style.backgroundColor='red';
 }
@@ -21,3 +23,26 @@ const makeBlueButton = document.getElementById('make-blue');
         purpleButton.onclick = function makePurple(){
             document.body.style.backgroundColor = 'purple';
         }
+
+
+// option 4
+const pinkButton = document.getElementById('make-pink');
+        // console.log(pinkButton);
+        pinkButton.addEventListener('click', makePink)
+
+        function makePink(){
+            document.body.style.backgroundColor = 'pink';
+        }
+
+// option 04.01
+const makeOliveButton = document.getElementById('make-olive');
+        makeOliveButton.addEventListener('click', function makeOlive(){
+            document.body.style.backgroundColor = 'olive';
+        })
+
+// option 04 final eta use korbo
+
+document.getElementById('make-goldenRod').addEventListener('click',function(){
+    document.body.style.backgroundColor = 'goldenRod';
+})
+
