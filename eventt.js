72cd1fd2de9ca5c8d62f0485c209aad357cd7759
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-12 17:38:37 +0600

    onclick

--- eventt.js
+++ eventt.js
@@ -7,4 +7,17 @@ function makeRed(){
 }
 
 
-// 
+// 0ption 03
+const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+        // console.log(makeBlueButton);
+        function makeBlue(){
+            document.body.style.backgroundColor = 'blue';}
+
+// another 03.01
+// const purpleButton = document.getElementById('make-purple');
+        // console.log(purpleButton);
+        const purpleButton = document.getElementById('make-purple');
+        purpleButton.onclick = function makePurple(){
+            document.body.style.backgroundColor = 'purple';
+        }
